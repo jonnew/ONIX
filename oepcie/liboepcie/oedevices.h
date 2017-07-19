143449581f3e88853898f9cbd4361d1dc350c0e0
Philip Dakin <pdak@umich.edu>
2017-07-19 07:47:34 -0400

    quick update to device specs

--- oepcie/liboepcie/oedevices.h
+++ oepcie/liboepcie/oedevices.h
@@ -30,9 +30,9 @@ typedef struct device{
  */
 
 static const device_t DEVICES[DEVICE_TYPES] = {
-	{.read_offset = 0, .read_size = ((16 + 3) * 32), 
+	{.read_offset = 0, .read_size = (16 * (32 + 3)), 
 		.write_offset = 0, .write_size = 0, .id = 0}, // RHD2032
-	{.read_offset = 0, .read_size = ((16 + 3) * 64), 
+	{.read_offset = 0, .read_size = (16 * (64 + 3)), 
 		.write_offset = 0, .write_size = 0, .id = 1}, // RHD2064
 	{.read_offset = 0, .read_size = (32 * 6), 
 		.write_offset = 0, .write_size = 0, .id = 2}, // MPU950
