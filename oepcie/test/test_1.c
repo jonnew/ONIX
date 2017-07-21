4a0bb98d86ee74e16ba06ee9eb5bbfd4aa75c94b
jonnew <jpnewman@mit.edu>
2017-07-21 10:48:39 -0400

    Formatting cleanup
    - Thinking about combining header and signal streams.

--- oepcie/test/test_1.c
+++ oepcie/test/test_1.c
@@ -6,6 +6,7 @@
 #include "unistd.h"
 
 #include "../liboepcie/oepcie.h"
+#include "../liboepcie/oedevice.h"
 
 #define FAKE_HEADER_STREAM_FID "../test/stream_files/header.bin"
 #define FAKE_CONFIG_STREAM_FID "../test/stream_files/header.bin"
@@ -16,7 +17,7 @@ int main(){
 
     // Generate context
     oe_ctx ctx = NULL;
-	ctx = oe_create_ctx();
+    ctx = oe_create_ctx();
 
     // Set default stream paths
     const char *header_path = FAKE_HEADER_STREAM_FID;
@@ -25,9 +26,9 @@ int main(){
     const char *signal_path = FAKE_SIGNAL_STREAM_FID;
 
     oe_set_ctx_opt(ctx, OE_HEADERSTREAMPATH, header_path, strlen(header_path) + 1);
-	oe_set_ctx_opt(ctx, OE_CONFIGSTREAMPATH, config_path, strlen(config_path) + 1);
-	oe_set_ctx_opt(ctx, OE_DATASTREAMPATH, data_path, strlen(data_path) + 1);
-	oe_set_ctx_opt(ctx, OE_SIGNALSTREAMPATH, signal_path, strlen(signal_path) + 1);
+    oe_set_ctx_opt(ctx, OE_CONFIGSTREAMPATH, config_path, strlen(config_path) + 1);
+    oe_set_ctx_opt(ctx, OE_DATASTREAMPATH, data_path, strlen(data_path) + 1);
+    oe_set_ctx_opt(ctx, OE_SIGNALSTREAMPATH, signal_path, strlen(signal_path) + 1);
 
     // Test oe_get_ctx_opt
     printf("\tHeader stream path set/get with too short buffer test...");
@@ -39,69 +40,78 @@ int main(){
     assert(rc == OE_EINVALARG);
     printf("pass.\n");
 
-	printf("\tHeader stream path set/get test...");
+    printf("\tHeader stream path set/get test...");
     char header_check[100];
-	size_t header_len = 100;
-	rc = oe_get_ctx_opt(ctx, OE_HEADERSTREAMPATH, header_check, &header_len);
+    size_t header_len = 100;
+    rc = oe_get_ctx_opt(ctx, OE_HEADERSTREAMPATH, header_check, &header_len);
     assert(rc == 0);
     assert(strcmp(header_path, header_check) == 0);
     assert(header_len == strlen(header_path) + 1);
     printf("pass.\n");
 
-	printf("\tConfig stream path set/get test...");
+    printf("\tConfig stream path set/get test...");
     char config_check[100];
     size_t config_len = 100;
-	rc = oe_get_ctx_opt(ctx, OE_CONFIGSTREAMPATH, config_check, &config_len);
+    rc = oe_get_ctx_opt(ctx, OE_CONFIGSTREAMPATH, config_check, &config_len);
     assert(rc == 0);
     assert(!strcmp(config_path, config_check));
     assert(config_len == strlen(config_path) + 1);
-	printf("pass.\n");
+    printf("pass.\n");
 
-	printf("\tData input stream path set/get test...");
+    printf("\tData input stream path set/get test...");
     char data_check[100];
     size_t data_len = 100;
-	rc = oe_get_ctx_opt(ctx, OE_DATASTREAMPATH, data_check, &data_len);
+    rc = oe_get_ctx_opt(ctx, OE_DATASTREAMPATH, data_check, &data_len);
     assert(rc == 0);
     assert(!strcmp(data_path, data_check));
     assert(data_len == strlen(data_path) + 1);
-	printf("pass.\n");
+    printf("pass.\n");
 
-	// Write our desired info into the header file
-	int config_id = 333;
-	int device_one_id = 0;
-	int device_two_id = 1;
-	int device_three_id = 3;
-	int neg_one = -1;
+    printf("\tSignal input stream path set/get test...");
+    char signal_check[100];
+    size_t signal_len = 100;
+    rc = oe_get_ctx_opt(ctx, OE_SIGNALSTREAMPATH, signal_check, &signal_len);
+    assert(rc == 0);
+    assert(!strcmp(signal_path, signal_check));
+    assert(signal_len == strlen(signal_path) + 1);
+    printf("pass.\n");
+
+    // Write our desired info into the header file
+    int config_id = 333;
+    int device_one_id = RHD2032;
+    int device_two_id = RHD2032;
+    int device_three_id = MPU950;
+    int neg_one = -1;
 
     // Generate fake header stream
     int fd_head = open(FAKE_HEADER_STREAM_FID, O_RDWR);
     printf("fd_head is %d\n", fd_head);
-	write(fd_head, &config_id, sizeof(int));
-	write(fd_head, &device_one_id, sizeof(int));
-	write(fd_head, &device_two_id, sizeof(int));
-	write(fd_head, &device_three_id, sizeof(int));
-	write(fd_head, &neg_one, sizeof(int));
+    write(fd_head, &config_id, sizeof(int));
+    write(fd_head, &device_one_id, sizeof(int));
+    write(fd_head, &device_two_id, sizeof(int));
+    write(fd_head, &device_three_id, sizeof(int));
+    write(fd_head, &neg_one, sizeof(int));
 
     // Initialize context
-	assert(oe_init_ctx(ctx) == 0);
+    assert(oe_init_ctx(ctx) == 0);
 
     int wack;
-	oe_write_reg(ctx, 0, 0x0000000A, 0xABCDEF01, &wack);
+    oe_write_reg(ctx, 0, 0x0000000A, 0xABCDEF01, &wack);
 
-	int fake_ack = 0x0000001;
-	int read_fake_val = 1776;
+    int fake_ack = 0x0000001;
+    int read_fake_val = 1776;
 
-	int fd_conf = open(FAKE_CONFIG_STREAM_FID, O_RDWR);
-	lseek(fd_conf, 16, SEEK_SET);
-	write(fd_conf, &fake_ack, sizeof(int));
-	write(fd_conf, &read_fake_val, sizeof(int));
+    int fd_conf = open(FAKE_CONFIG_STREAM_FID, O_RDWR);
+    lseek(fd_conf, 16, SEEK_SET);
+    write(fd_conf, &fake_ack, sizeof(int));
+    write(fd_conf, &read_fake_val, sizeof(int));
 
-	int val;
+    int val;
     int rack;
-	oe_read_reg(ctx, 0, 0x0000000A, &val, &rack);
-	printf("** Read value %d\n", val);
+    oe_read_reg(ctx, 0, 0x0000000A, &val, &rack);
+    printf("** Read value %d\n", val);
 
-	oe_destroy_ctx(ctx);
+    oe_destroy_ctx(ctx);
 
-	return 1;
+    return 1;
 }
