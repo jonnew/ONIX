902bce5850fce26bb4cce9888fc4881ea2a21806
jonnew <jpnewman@mit.edu>
2017-09-16 13:24:37 -0400

    More work on liboepcie
    - More complete host/firmware test programs
    - Simplification and bug fixes in library code
    - Realizing that a context needs to hold a small state machine to govern
    run state transitions from idle to running to acquiring and back. This
    state dictates what read/write operations are permitted. e.g. is is not
    cool to reset the sample rate in anything but the idle state. trying to
    oe_read() during the idle state will block forever, etc

--- oepcie/liboepcie/oepcie.h
+++ oepcie/liboepcie/oepcie.h
@@ -11,13 +11,15 @@ typedef enum oe_ctx_opt {
     OE_DATASTREAMPATH,
     OE_SIGNALSTREAMPATH,
     OE_DEVICEMAP,
-    OE_NUMDEVICES
+    OE_NUMDEVICES,
+    OE_READFRAMESIZE,
+    OE_WRITEFRAMESIZE
 } oe_ctx_opt_t;
 
 // NB: If you add an error here, make sure to update the oe_error_string array
 // in oepcie.c
 typedef enum oe_error {
-    OE_ESUCCESS         = 0,   // Success
+    OE_ESUCCESS         =  0,  // Success
     OE_EPATHINVALID     = -1,  // Invalid stream path, fail on open
     OE_EREINITCTX       = -2,  // Double initialization attempt
     OE_EDEVID           = -3,  // Invalid device ID on init or reg op
@@ -35,26 +37,13 @@ typedef enum oe_error {
     OE_EBUFFERSIZE      = -15, // Supplied buffer is too small
     OE_EBADDEVMAP       = -16, // Badly formated device map supplied by firmware
     OE_EBADALLOC        = -17, // Bad dynamic memory allocation
-    OE_MINERRORNUM
+    OE_ECLOSEFAIL       = -18, // File descriptor close failure, check errno
+    OE_EDATATYPE        = -19, // Invalid underlying data types
+    OE_EREADONLY        = -20, // Attempted write to read only object (register, context option, etc)
+
+    // NB: Always at bottom
+    OE_MINERRORNUM =    -21
 } oe_error_t;
-
-typedef enum oe_signal {
-    OE_NULLSIG,
-    OE_CONFIGNACK,             // Configuration no-acknowledgement
-    OE_CONFIGWACK,             // Configuration write-acknowledgement
-    OE_CONFIGRACK,             // Configuration read-acknowledgement
-    OE_HEADERSTART,            // Header start marker
-    OE_DEVICEINST,             // Deivce instance
-    OE_HEADEREND,              // Header end marker
-} oe_signal_t;
-
-typedef enum oe_config_reg_offset {
-    OE_CONFDEVID        = 0,   // Configuration device id register byte offset
-    OE_CONFADDR         = 4,   // Configuration register address register byte offset
-    OE_CONFVALUE        = 8,   // Configuration register value register byte offset
-    OE_CONFRW           = 12,  // Configuration register read/write register byte offset
-    OE_CONFTRIG         = 13,  // Configuration read/write trigger register byte offset
-} oe_config_reg_offset_t;
 
 // Context
 typedef struct oe_ctx_impl *oe_ctx;
@@ -64,24 +53,16 @@ oe_ctx oe_create_ctx();
 int oe_init_ctx(oe_ctx ctx);
 int oe_close_ctx(oe_ctx ctx);
 int oe_destroy_ctx(oe_ctx ctx);
-int oe_set_ctx_opt(oe_ctx ctx, oe_ctx_opt_t option, const void* option_value, size_t option_len);
-int oe_get_ctx_opt(const oe_ctx ctx, oe_ctx_opt_t option, void* option_value, size_t *option_len);
+int oe_set_ctx_opt(oe_ctx ctx, const oe_ctx_opt_t option, const void* option_value, size_t option_len);
+int oe_get_ctx_opt(const oe_ctx ctx, const oe_ctx_opt_t option, void* option_value, size_t *option_len);
 
 // Hardware inspection and manipulation
-int oe_read_reg(const oe_ctx ctx, int32_t device_idx, uint32_t addr, uint32_t *value);
-int oe_write_reg(const oe_ctx ctx, int32_t device_idx, uint32_t addr, uint32_t value);
+int oe_read_reg(const oe_ctx ctx, const oe_dev_idx_t device_idx, const oe_reg_addr_t addr, oe_reg_val_t *value);
+int oe_write_reg(const oe_ctx ctx, const oe_dev_idx_t device_idx, const oe_reg_addr_t addr, const oe_reg_val_t value);
 int oe_read(const oe_ctx ctx, void *data, size_t size);
 
-// Error inspection
+// Internal type conversion
 void oe_error(oe_error_t err, char *str, size_t str_len);
-
-// Static helpers
-static inline int _oe_read(int fd, void* data, size_t size);
-static inline int _oe_read_signal_packet(int signal_fd, uint8_t *buffer);
-static int _oe_read_signal_type(int signal_fd, oe_signal_t *type);
-static int _oe_read_signal_data(int signal_fd, oe_signal_t *type, void *data, size_t *size);
-static int _oe_pump_signal_type(int signal_fd, const oe_signal_t type);
-static int _oe_pump_signal_data(int signal_fd, const oe_signal_t type, void *data, size_t *size);
-static int _oe_cobs_unstuff(uint8_t *dst, const uint8_t *src, size_t size);
+int oe_device(const oe_device_id_t dev_id, char *str, size_t str_len);
 
 #endif
--- oepcie/test/host128.c
+++ oepcie/test/host128.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -8,17 +9,19 @@
 #include "../liboepcie/oedevice.h"
 #include "../liboepcie/oepcie.h"
 
-// TODO: This should be retrieved from header packet 
-// Params
-const size_t num_chan  = 128;
-const size_t samp_per_chan_per_block = 1000;
-const size_t fs_hz = 30e3;
-
 int main()
 {
+    // Error string buffer
+    const size_t elen = 100;
+    char ebuf[elen];
+
+    // Frames per block read
+    const size_t frames_per_read = 100;
+
     // Generate context
     oe_ctx ctx = NULL;
     ctx = oe_create_ctx();
+    if (!ctx) exit(EXIT_FAILURE);
 
     // Set stream paths
     const char *config_path = "/tmp/rat128_config";
@@ -34,15 +37,8 @@ int main()
     oe_set_ctx_opt(ctx, OE_SIGNALSTREAMPATH, sig_path, strlen(sig_path) + 1);
     oe_set_ctx_opt(ctx, OE_DATASTREAMPATH, data_path, strlen(data_path) + 1);
 
-    // TODO: Build from header packet constants
-    //oe_write_config();    
-    const size_t block_bytes = num_chan * samp_per_chan_per_block * 2;
-
     // Initialize context and discover hardware
-    int rc = oe_init_ctx(ctx);
-    printf("Return %d\n", rc);
-
-    //assert(oe_init_ctx(ctx) == 0);
+    assert(oe_init_ctx(ctx) == 0);
 
     // Examine device map
     oe_size_t num_devs = 0;
@@ -57,31 +53,103 @@ int main()
     int dev_idx;
     for (dev_idx = 0; dev_idx < num_devs; dev_idx++) {
 
-        printf("\t%d) ID: %d, Offset: %zu, Read size:%zu\n",
+        char dev_str[80];
+        oe_device(devices[dev_idx].id, dev_str, 80);
+
+        printf("\t%d) ID: %d (%s), Offset: %u, Read size:%u\n",
                dev_idx,
                devices[dev_idx].id,
+               dev_str,
                devices[dev_idx].read_offset,
                devices[dev_idx].read_size);
     }
 
+    oe_size_t frame_size = 0;
+    size_t frame_size_sz = sizeof(frame_size);
+    oe_get_ctx_opt(ctx, OE_READFRAMESIZE, &frame_size, &frame_size_sz); 
+    printf("Frame size: %u bytes\n", frame_size);
+
     // Start acquisition
-    oe_write_reg(ctx, OEPCIEMASTER, OEPCIEMASTER_RUNNING, 1);
+    // Try to write to base clock freq, which is write only
+    int rc = oe_write_reg(ctx, OE_PCIEMASTERDEVIDX, OE_PCIEMASTERDEV_BASECLKHZ, 10e6);
+    assert(rc == OE_EREADONLY && "Succesful write to read-only register.");
 
-    char buffer[8 + block_bytes];
-    while (oe_read(ctx, &buffer, 8 + block_bytes)  >= 0)  {
+    uint32_t base_hz;
+    rc = oe_read_reg(ctx, OE_PCIEMASTERDEVIDX, OE_PCIEMASTERDEV_BASECLKHZ, &base_hz);
+    if (rc) {
+        oe_error(rc, ebuf, elen);
+        printf("%s\n", ebuf);
+    }
+    assert(!rc && "Register read failure.");
+    
+    //  Calculate m and d to get 10kHz
+    uint32_t fs_desired = 100;
+    uint32_t m = 1;
+    uint32_t d = (m * base_hz) / fs_desired;
 
-        // Get sample number and print
-        uint64_t sample = *(uint64_t *)buffer;
-        printf("Sample: %" PRIu64 "\n", sample);
+    // Set clock divider to 10/100 to get 10kHz sample clock
+    rc = oe_write_reg(ctx, OE_PCIEMASTERDEVIDX, OE_PCIEMASTERDEV_CLKM, m);
+    if (rc) {
+        oe_error(rc, ebuf, elen);
+        printf("Error: %s\n", ebuf);
+    }
+    assert(!rc && "Register write failure.");
 
-        // LFP data
-        int16_t *lfp = (int16_t *)(buffer + 8);
-        int i;
-        printf("LFP: [");
-        for (i =0; i < 10; i++)
-            printf("%" PRId16 " ", *(lfp + i));
-        printf("...]\n");
+    rc = oe_write_reg(ctx, OE_PCIEMASTERDEVIDX, OE_PCIEMASTERDEV_CLKD, d);
+    if (rc) {
+        oe_error(rc, ebuf, elen);
+        printf("Error: %s\n", ebuf);
+    }
+    assert(!rc && "Register write failure.");
 
+    uint32_t fs_hz;
+    rc = oe_read_reg(ctx, OE_PCIEMASTERDEVIDX, OE_PCIEMASTERDEV_FSCLKHZ, &fs_hz);
+    if (rc) {
+        oe_error(rc, ebuf, elen);
+        printf("%s\n", ebuf);
+    }
+    assert(!rc && "Register read failure.");
+
+    assert(fs_hz == fs_desired && "Sample rate set failed.");
+
+    // Start acquisition
+    rc = oe_write_reg(ctx, OE_PCIEMASTERDEVIDX, OE_PCIEMASTERDEV_RUNNING, 1);
+    if (rc) {
+        oe_error(rc, ebuf, elen);
+        printf("%s\n", ebuf);
+    }
+
+    const size_t read_size = frame_size * frames_per_read;
+    uint8_t buffer[frame_size * frames_per_read];
+
+    while (oe_read(ctx, &buffer, read_size)  >= 0)  {
+
+        printf("-- start read block --\n");
+
+        int f;
+        for (f = 0; f < frames_per_read; f++) {
+
+            size_t frame_offset = f * frame_size;
+            uint64_t sample = *(uint64_t *)(buffer + frame_offset);
+
+            printf("Sample: %" PRIu64 "\n", sample);
+
+            for (dev_idx = 0; dev_idx < num_devs; dev_idx++) {
+                
+                oe_device_t this_dev = devices[dev_idx];
+                
+                char dev_name[20] = {0};
+                oe_device(this_dev.id, dev_name, 20);
+
+                printf("\tDev: %d (%s)" PRIu64 "\n", dev_idx, dev_name);
+                int16_t *lfp = (int16_t *)((uint8_t *)buffer + this_dev.read_offset);
+                int i;
+                printf("\tData: [");
+                for (i = 0; i < 10; i++)
+                    printf("%" PRId16 " ", *(lfp + i));
+                printf("...]\n");
+            }
+        }
     }
 
     oe_destroy_ctx(ctx);
